3378fea26c85c16598852def16db06f11853c836
Eli Hammond <ebaal8226@gmail.com>
2025-01-08 12:15:06 +0000

    Set up portfolio models and admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=254)
+    website_url = models.URLField(max_length=1250, null=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
