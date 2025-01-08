06e24382dbdecde44eee6cb404936b7954d4186b
Eli Hammond <ebaal8226@gmail.com>
2025-01-08 13:33:16 +0000

    Create view for portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Portfolio
 
 # Create your views here.
+
+def all_portfolio(request):
+    portfolio = Portfolio.objects.all()
+
+    return render(request, "portfolio/portfolio.html", portfolio)
