0dc9702f71c055763d75f14384761082076f0aab
Eli Hammond <ebaal8226@gmail.com>
2025-01-08 13:02:26 +0000

    Update plural name in admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Portfolio(models.Model):
+    class Meta:
+        verbose_name_plural = "Portfolio"
+
     name = models.CharField(max_length=254)
     website_url = models.URLField(max_length=1250, null=True, blank=True)
     description = models.TextField()
