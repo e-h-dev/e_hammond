50153eb29d9ff98307f4a40d4d607fbf1967eaf6
Eli Hammond <ebaal8226@gmail.com>
2025-01-12 14:32:00 +0000

    Create models for reviews

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+
+    # name = models.CharField(max_length=254)
+    review = models.TextField()
+    rating = models.IntegerField(default=1,
+                                 choices=((i, i) for i in range(1, 6)))
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
