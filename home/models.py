579e6d8d5ad15e5c5c977f443dd17a1e0bd77d14
Eli Hammond <ebaal8226@gmail.com>
2025-01-13 14:27:18 +0000

    Recreate migrations fo fix broken database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
     
+
+class Reviews(models.Model):
+    class Meta:
+        verbose_name_plural = "Reviews"
+
+
+    name = models.CharField(max_length=254)
+    rating = models.IntegerField(default=1,
+                                 choices=((i, i) for i in range(1, 6)))
+    review = models.TextField(max_length=600, null=True, blank=True)
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Reviews
 
 # Create your views here.
 
 def index(request):
+
+    reviews = Reviews.objects.all()
+
+    context = {
+        'reviews': reviews,
+    }
     
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
