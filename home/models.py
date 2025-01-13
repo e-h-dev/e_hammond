86f42bb7bb05836c3e9fe7c394aa7a030ce179bb
Eli Hammond <ebaal8226@gmail.com>
2025-01-13 12:20:18 +0000

    Remove review functionality to recreate

--- home/models.py
+++ home/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
 
-class Review(models.Model):
-
-
-    name = models.CharField(max_length=36, blank=True, null=True)
-    review = models.TextField()
-    rating = models.IntegerField(default=1,
-                                 choices=((i, i) for i in range(1, 6)))
-    date = models.DateField(auto_now=True)
-    time = models.TimeField(auto_now=True)
-
-    def __str__(self):
-        return self.review
