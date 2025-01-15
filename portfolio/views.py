c2cbf071c6e691b697a57d4a0e7b73329ed5be5e
Eli Hammond <ebaal8226@gmail.com>
2025-01-15 10:14:31 +0000

    Update main portfolio view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Portfolio
 
 # Create your views here.
 
 def all_portfolio(request):
+    """
+    A view to dispaly all uploaded portfolios, images,
+    links and information.
+    """
     portfolio = Portfolio.objects.all()
 
     context = {
@@ -11,3 +15,8 @@ def all_portfolio(request):
     }
 
     return render(request, "portfolio/portfolio.html", context)
+
+
+# def full_description(request, id):
+
+#     portfolio = get_object_or_404(Portfolio, id)
