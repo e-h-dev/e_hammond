4a6acf0fb70927c51ccc5d7e2087bffca49de288
Eli Hammond <ebaal8226@gmail.com>
2025-01-16 14:59:18 +0000

    Create contacts app and build models.py

---
+++ contacts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Contacts(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Contacts"
+
+    name = models.CharField(max_length=26)
+    subject = models.CharField(max_length=52, null=True, blank=True)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    message = models.TextField()
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    read = models.BooleanField(default=False)
+    
