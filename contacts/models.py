b40cf298f65c02dd2ab37aa6059baccc3fa15f67
Eli Hammond <ebaal8226@gmail.com>
2025-01-20 12:10:27 +0000

    Add sent message to in models craete if statement to only display to user

--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Contacts(models.Model):
         verbose_name_plural = "Contacts"
 
     name = models.CharField(max_length=26)
+    send_to = models.ForeignKey(User, on_delete=models.CASCADE, default=True)
     subject = models.CharField(max_length=52, null=True, blank=True)
     email = models.EmailField()
     phone = models.IntegerField()
