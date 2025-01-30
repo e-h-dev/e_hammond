9c205fa06fde3d554a4729b1a00c8b681331b900
Eli Hammond <ebaal8226@gmail.com>
2025-01-30 14:44:45 +0000

    Upate dcontext to display number of replied messages

--- contacts/contexts.py
+++ contacts/contexts.py
@@ -1,25 +1,18 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
-
-
-from .models import Contacts
+from .models import Contacts, Replied
 
 
 def contacts_inbox(request):
-
-    # user = User.objects.all()
-
-    # current_user = user.filter(username='johndoe')
-
-    # print(current_user)
-
-    #user = get_object_or_404(User, username=request.user)
 
     user = request.user.id
     if user:
         user_inbox = get_object_or_404(User, username=request.user)
         inbox = Contacts.objects.filter(send_to=user_inbox)
+        replied = Replied.objects.filter(send_to=user_inbox)
+
+        replied_count = len(replied)
 
         unread_messages = inbox.filter(read=False).count()
 
@@ -33,7 +26,8 @@ def contacts_inbox(request):
         context = {
             'inbox': inbox,
             'message_count': message_count,
-            'unread_messages': unread_messages
+            'unread_messages': unread_messages,
+            'replied_count': replied_count
         }
     else:
         inbox = Contacts.objects.all()
