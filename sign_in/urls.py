433ecc09842202ebbae09626cab002986813e059
e-h-dev <ebaal8226@gmail.com>
2025-02-17 19:09:22 +0000

    Setup custom sign in functionality

--- sign_in/urls.py
+++ sign_in/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.sign_in, name='sign_in'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('sign_out', views.sign_out, name='sign_out'),
 ]
--- sign_in/views.py
+++ sign_in/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def sign_in(request):
     return render(request, 'sign_in/sign_in.html')
+
+
+def sign_up(request):
+    return render(request, 'sign_in/sign_up.html')
+
+
+def sign_out(request):
+    return render(request, 'sign_in/sign_out.html')
