eef101ef3dec9c6177e0b64c71a53b9442d7556a
e-h-dev <ebaal8226@gmail.com>
2025-03-26 10:08:48 +0000

    Update allre lavant settings for deployment

--- e_hammond/settings.py
+++ e_hammond/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-if os.path.exists("env.py"):
+if os.path.isfile("env.py"):
     import env
 
 
@@ -31,7 +31,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-e-h-dev-e-hammond-2azd4pvsvj.app.codeanywhere.com']
+ALLOWED_HOSTS = [
+    '8000-e-h-dev-e-hammond-2azd4pvsvj.app.codeanywhere.com',
+    'e-hammond-7f6468517ddb.herokuapp.com'
+    ]
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -115,16 +118,19 @@ WSGI_APPLICATION = 'e_hammond.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'DATABASE_URL' in os.environ:
+
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
