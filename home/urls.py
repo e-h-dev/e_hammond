9ca14ca96c3a64281efd8e2e488fd05bf7226f8a
e-h-dev <ebaal8226@gmail.com>
2025-04-15 22:08:31 +0000

    Update messaging functionality for none users

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from contacts import views as contact_views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('review/', views.create_review, name='create_review'),
+    path('compose/', contact_views.compose_message, name='compose_message'),
 ]
